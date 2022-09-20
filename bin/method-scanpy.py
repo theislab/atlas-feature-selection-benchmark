2a04757c87437c513749f9ddaebf473ce7f495af
Oliver Dietrich <Oliver.Dietrich@helmholtz-hiri.de>
2022-09-20 10:06:53 +0200

    Remove data/ from .gitignore. Moved batch from <bool> to flag. Added shorthands. If statement to skip normalization for seurat_v3.

--- bin/method-scanpy.py
+++ bin/method-scanpy.py
@@ -8,12 +8,10 @@ Usage:
 
 Options:
     -h --help            Show this screen.
-    --out-file=<path>    Path to output file.
-    --n-features=<int>   Number of features to select [default: 1000].
-    --flavor=<str>       Flavor of feature selection method.
-    One of: seurat, cell_ranger, seurat_v3 [default: seurat].    
-    --batch=<bool>        Whether to apply to each batch.
-    Requires Batch in .obs [default: False].
+    -o --out-file=<path>    Path to output file.
+    -n --n-features=<int>   Number of features to select [default: 1000].
+    -f --flavor=<str>       Flavor of feature selection method. One of: seurat, cell_ranger, seurat_v3 [default: seurat].    
+    -b --batch              Apply to each batch. Requires column 'Batch' in .obs.
 """
 
 def select_features_scanpy(adata, n, flavor, batch):
@@ -29,12 +27,14 @@ def select_features_scanpy(adata, n, flavor, batch):
     flavor
     	Method flavor ('seurat', 'cell_ranger', 'seurat_v3')
     batch
-    	Boolean whether to use 'Batch' as batch_key
+    	Flag whether to use 'Batch' as batch_key
 
     Returns
     ----------
     DataFrame containing the selected features
     """
+
+    import scanpy as sc
 
     if adata.n_vars < n:
         import warnings
@@ -43,16 +43,16 @@ def select_features_scanpy(adata, n, flavor, batch):
             "Number of features to select is greater than the number present, setting n to adata.n_vars"
         )
         n = adata.n_vars
-
-    import scanpy as sc
     
     if batch:
         batch_key='Batch'
     else:
         batch_key=None
     
-    sc.pp.normalize_total(adata, target_sum=1e4)
-    sc.pp.log1p(adata)
+    if flavor in ["seurat", "cell_ranger"]:
+        sc.pp.normalize_total(adata, target_sum=1e4)
+        sc.pp.log1p(adata)
+        
     sc.pp.highly_variable_genes(adata, n_top_genes=n, flavor=flavor, batch_key=batch_key)
     
     adata.var["Feature"] = adata.var.index
