08d7ef625d41c69b6ff1c54e99902450c9979a7c
Luke Zappia <luke.zappia@helmholtz-muenchen.de>
2022-11-30 15:28:51 +0100

    Move DUBStepR installation to main script

--- bin/method-DUBStepR.R
+++ bin/method-DUBStepR.R
@@ -22,6 +22,19 @@ suppressMessages({
     source("_functions.R")
 })
 
+#' Install DUBStepR from GitHub using remotes
+install_DUBStepR <- function() {
+    if (!requireNamespace("DUBStepR", quietly = TRUE)) {
+        message("Installing DUBStepR...")
+        remotes::install_github(
+            "prabhakarlab/DUBStepR@76aa39485742d5f5bcfb86346a8a1784ee08f6b9",
+            dependencies = FALSE
+        )
+    } else {
+        message("DUBStepR already installed")
+    }
+}
+
 #' Select features using DUBStepR
 #'
 #' @param input Seurat object
@@ -46,6 +59,8 @@ main <- function() {
     args <- docopt::docopt(doc)
     file <- args[["<file>"]]
     out_file <- args[["--out-file"]]
+
+    install_DUBStepR()
 
     message("Reading data from '", file, "'...")
     input <- read_h5ad(
